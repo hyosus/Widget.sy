0d30d850f74dcaa8b049e4f942db3faceda6866b
hyosus <shaniceyeong@gmail.com>
2025-02-10 14:18:19 +0800

    clean up main screen

--- app/src/main/java/com/example/widgetsy/MainActivity.kt
+++ app/src/main/java/com/example/widgetsy/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.widgetsy
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
 import android.provider.Settings
 import android.util.Log
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,6 +33,7 @@ import androidx.compose.runtime.remember
 import com.example.widgetsy.musicWidget.SpotifyService
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
@@ -36,6 +41,11 @@ import androidx.compose.ui.unit.dp
 import androidx.palette.graphics.Palette
 import com.example.widgetsy.ui.theme.WeatherAppTheme
 import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationResponse
+import com.spotify.sdk.android.auth.LoginActivity.REQUEST_CODE
 
 
 class MainActivity : ComponentActivity() {
@@ -47,12 +57,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         spotifyService = SpotifyService(this)
+        // Add WebView debugging here
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
 
         setContent {
             WeatherAppTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize().padding(16.dp),
-                ) { innerPadding ->
+                Scaffold()
+                { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)) {
                         CurrentTrackDisplay(spotifyService)
                     }
@@ -64,9 +77,15 @@ class MainActivity : ComponentActivity() {
         requestIgnoreBatteryOptimizations(this)
     }
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d("AuthFlow", "Received intent: ${intent.data}") // Log redirect URI
+    }
+
     override fun onStart() {
         super.onStart()
-        spotifyService.connectSpotifyAppRemote()
+        // Only connect if already authorized
+        spotifyService.authenticateSpotify(this)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -86,58 +105,63 @@ class MainActivity : ComponentActivity() {
             context.startActivity(intent)
         }
     }
+}
+fun getPrimaryColorFromImage(bitmap: Bitmap): Int {
+    val palette = Palette.from(bitmap).generate()
+    return palette.getDominantColor(0) // Default color if no dominant color is found
+}
 
-    fun getPrimaryColorFromImage(bitmap: Bitmap): Int {
-        val palette = Palette.from(bitmap).generate()
-        return palette.getDominantColor(0) // Default color if no dominant color is found
+@Composable
+fun CurrentTrackDisplay(spotifyService: SpotifyService) {
+    val currentTrack by spotifyService.currentTrack
+    var trackImage by remember { mutableStateOf<ImageBitmap?>(null) }
+    var backgroundColor by remember { mutableStateOf(Color.White) }
+
+    LaunchedEffect(currentTrack) {
+        spotifyService.getTrackImage { imageBitmap ->
+            trackImage = imageBitmap
+            imageBitmap?.let {
+                backgroundColor = Color(getPrimaryColorFromImage(it.asAndroidBitmap()))
+                Log.d("MainActivity", "Background color: $backgroundColor")
+            }
+        }
     }
 
-    @Composable
-    fun CurrentTrackDisplay(spotifyService: SpotifyService) {
-        val currentTrack by spotifyService.currentTrack
-        var trackImage by remember { mutableStateOf<ImageBitmap?>(null) }
-        var backgroundColor by remember { mutableStateOf(Color.White) }
+    Column(
+        modifier = Modifier.fillMaxSize().background(backgroundColor).padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)) {
 
-        LaunchedEffect(currentTrack) {
-            spotifyService.getTrackImage { imageBitmap ->
-                trackImage = imageBitmap
-                imageBitmap?.let {
-                    backgroundColor = Color(getPrimaryColorFromImage(it.asAndroidBitmap()))
-                    Log.d("MainActivity", "Background color: $backgroundColor")
-                }
-            }
+        trackImage?.let {
+            Image(
+                bitmap = it,
+                contentDescription = "Album Image"
+            )
+
+            Log.d("MainActivity", "Image bitmap: $it")
         }
+        Text(
+            text = currentTrack?.name ?: "No track playing",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp)
+        Text(text = currentTrack?.artist?.name ?: "")
 
-        Column(modifier = Modifier.fillMaxSize().background(backgroundColor).padding(16.dp)) {
-            Text(text = "Current Track:")
-            Text(text = currentTrack?.name ?: "No track playing")
-            Text(text = currentTrack?.artist?.name ?: "")
-            trackImage?.let {
-                Image(
-                    bitmap = it,
-                    contentDescription = "Album Image"
-                )
-
-                Log.d("MainActivity", "Image bitmap: $it")
-            }
-
-            Row {
-                Button(onClick = {
-                    spotifyService.skipToPrevious()
-                }) {
-                    Text(text = "Previous")
-                }
-                Button(onClick = {
-                    spotifyService.pause()
-                }) {
-                    Text(text = "Pause")
-                }
-                Button(onClick = {
-                    spotifyService.skipToNext()
-                }) {
-                    Text(text = "Next")
-                }
-            }
-        }
+//        Row {
+//            Button(onClick = {
+//                spotifyService.skipToPrevious()
+//            }) {
+//                Text(text = "Previous")
+//            }
+//            Button(onClick = {
+//                spotifyService.pause()
+//            }) {
+//                Text(text = "Pause")
+//            }
+//            Button(onClick = {
+//                spotifyService.skipToNext()
+//            }) {
+//                Text(text = "Next")
+//            }
+//        }
     }
 }
